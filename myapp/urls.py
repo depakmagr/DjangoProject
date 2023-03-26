309fd2485b682818aa034468bd23ab72509e86f8
depakmagr <magardeepak606@gmail.com>
2023-03-26 21:35:01 +0545

    Second commit of the project

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import home
+from .views import *    # relative import.
 
 
 urlpatterns = [
-    path("", home),
+    path("", index),
+    path("template/", template),
+    path("students/", students),
+    # path("inside-template/", inside_template),
+    path("<str:name>/", name_func),
+    path("home", home),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,73 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+from django.template import loader
 
 
 def home(request):
-    return HttpResponse("<h1>Hello Deeps!!</h1>")
+    # print(dir(request))
+    print(request.method)
+    print(request.headers)
+    print(request.is_secure())
+    print(request.scheme)
+    name = (request.GET.get('name'))
+    age = (request.GET.get('age'))
+    return HttpResponse(f"<h1>Hello my name is {name} and I am {age} years old.</h1>")
+
+
+def name_func(request, name):
+    data = {
+        "deepak": "DEEPAK BAHADUR",
+        "gabriel": "GABRIEL KUMAR"
+    }
+    # try:
+    #     name = data[name]
+    # except KeyError:
+    #     pass
+    name = data.get(name)
+    if not name:
+        return HttpResponseNotFound("Invalid URL")
+    return HttpResponse(f"<h1>Hello i am {name}!</h1>")
+
+
+def template(request):
+    template = loader.get_template("home.html")
+    context = {"name": "Deeps", "age": 234, "hobbies": ['Sports', 'Reading', 'Movies']}
+    template_data = template.render(context, request)
+    return HttpResponse(template_data)
+    # return render(request, template_name="myapp/home.html")
+
+
+# def inside_template(request):
+#     return render(request, template_name="myapp/inside.html")
+
+
+def students(request):
+    context = {
+        "Infos": [
+        {
+            "name": "Deepak Magar",
+            "age": 25,
+            "department": "CA",
+        },
+        {
+            "name": "Gabriel Muktan",
+            "age": 16,
+            "department": "BCA",
+        },
+        {
+            "name": "Nobel Rai",
+            "age": 21,
+            "department": "CSA",
+        },
+        {
+            "name": "Prakash Magar",
+            "age": 29,
+            "department": "ICA",
+        }
+        ]
+    }
+    return render(request, "students.html", context)
+
+
+def index(request):
+    return render(request, template_name="index.html")
