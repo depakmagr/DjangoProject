8bb0f142ceea6da4aec2c9dd359838c906ef1b26
depakmagr <magardeepak606@gmail.com>
2023-04-11 16:25:52 +0545

    built login and setup

--- crud/views.py
+++ crud/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import *
 
 
 # Create your views here.
+@login_required
 def home(request):
     context = {"persons": Person.objects.all()}
     return render(request, template_name="crud/person.html", context=context)
